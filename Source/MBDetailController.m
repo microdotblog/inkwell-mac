caab49b8e7686104a6501ef21141b4046d85787c
Manton Reece <manton@me.com>
2026-03-29 13:31:12 +0200

    Refactored webview subclass into its own file.

--- Source/MBDetailController.m
+++ Source/MBDetailController.m
@@ -7,6 +7,7 @@
 
 #import "MBDetailController.h"
 #import "MBClient.h"
+#import "MBDetailWebView.h"
 #import "MBEntry.h"
 #import "MBHighlight.h"
 #import "MBLinkHoverBubble.h"
@@ -32,148 +33,11 @@ static NSString* const InkwellReaderHighlightLightBackgroundHex = @"#FFF9D6";
 static NSString* const InkwellReaderHighlightDarkBackgroundHex = @"#A96733";
 static NSString* const InkwellPreferencesDarkBlueBackgroundHex = @"#1c2435";
 static NSString* const InkwellPreferencesBlackBackgroundHex = @"#000000";
-static NSInteger const InkwellDetailDeleteHighlightContextMenuItemTag = 7100;
-static NSInteger const InkwellDetailHighlightContextMenuItemTag = 7101;
-static NSInteger const InkwellDetailHighlightContextMenuSeparatorTag = 7102;
 static CGFloat const InkwellDetailLinkBubbleHorizontalInset = 14.0;
 static CGFloat const InkwellDetailLinkBubbleBottomInset = 14.0;
 static CGFloat const InkwellDetailLinkBubbleHorizontalPadding = 12.0;
 static CGFloat const InkwellDetailLinkBubbleVerticalPadding = 6.0;
 static CGFloat const InkwellDetailLinkBubbleMaxWidth = 450.0;
-
-@interface MBDetailWebView : WKWebView
-
-@property (copy, nullable) BOOL (^focusSidebarHandler)(void);
-@property (copy, nullable) BOOL (^scrollPageUpHandler)(void);
-@property (copy, nullable) BOOL (^scrollPageDownHandler)(void);
-@property (copy, nullable) void (^deleteHoveredHighlightHandler)(void);
-@property (copy, nullable) BOOL (^shouldShowHighlightMenuItemHandler)(void);
-@property (copy, nullable) BOOL (^shouldShowDeleteHighlightMenuItemHandler)(void);
-
-@end
-
-@implementation MBDetailWebView
-
-- (void) keyDown:(NSEvent*) event
-{
-	NSString* characters = event.charactersIgnoringModifiers ?: @"";
-	if (characters.length > 0) {
-		unichar key_code = [characters characterAtIndex:0];
-		NSEventModifierFlags modifier_flags = (event.modifierFlags & NSEventModifierFlagDeviceIndependentFlagsMask);
-		BOOL has_disallowed_modifiers = ((modifier_flags & (NSEventModifierFlagCommand | NSEventModifierFlagOption | NSEventModifierFlagControl | NSEventModifierFlagShift)) != 0);
-		BOOL is_left_arrow_key = (key_code == NSLeftArrowFunctionKey);
-		if (!has_disallowed_modifiers && is_left_arrow_key && self.focusSidebarHandler != nil && self.focusSidebarHandler()) {
-			return;
-		}
-	}
-
-	[super keyDown:event];
-}
-
-- (void) scrollPageUp:(id) sender
-{
-	if (self.scrollPageUpHandler != nil && self.scrollPageUpHandler()) {
-		return;
-	}
-
-	[super scrollPageUp:sender];
-}
-
-- (void) scrollPageDown:(id) sender
-{
-	if (self.scrollPageDownHandler != nil && self.scrollPageDownHandler()) {
-		return;
-	}
-
-	[super scrollPageDown:sender];
-}
-
-- (void) scrollLineUp:(id) sender
-{
-	if (self.scrollPageUpHandler != nil && self.scrollPageUpHandler()) {
-		return;
-	}
-
-	[super scrollLineUp:sender];
-}
-
-- (void) scrollLineDown:(id) sender
-{
-	if (self.scrollPageDownHandler != nil && self.scrollPageDownHandler()) {
-		return;
-	}
-
-	[super scrollLineDown:sender];
-}
-
-- (void) willOpenMenu:(NSMenu*) menu withEvent:(NSEvent*) event
-{
-	[super willOpenMenu:menu withEvent:event];
-	#pragma unused(event)
-
-	if (menu == nil) {
-		return;
-	}
-
-	while ([menu indexOfItemWithTag:InkwellDetailDeleteHighlightContextMenuItemTag] != -1) {
-		NSInteger existing_index = [menu indexOfItemWithTag:InkwellDetailDeleteHighlightContextMenuItemTag];
-		[menu removeItemAtIndex:existing_index];
-	}
-
-	while ([menu indexOfItemWithTag:InkwellDetailHighlightContextMenuItemTag] != -1) {
-		NSInteger existing_index = [menu indexOfItemWithTag:InkwellDetailHighlightContextMenuItemTag];
-		[menu removeItemAtIndex:existing_index];
-	}
-
-	while ([menu indexOfItemWithTag:InkwellDetailHighlightContextMenuSeparatorTag] != -1) {
-		NSInteger existing_index = [menu indexOfItemWithTag:InkwellDetailHighlightContextMenuSeparatorTag];
-		[menu removeItemAtIndex:existing_index];
-	}
-
-	BOOL should_show_delete_highlight_item = NO;
-	if (self.shouldShowDeleteHighlightMenuItemHandler != nil) {
-		should_show_delete_highlight_item = self.shouldShowDeleteHighlightMenuItemHandler();
-	}
-
-	BOOL should_show_highlight_item = NO;
-	if (self.shouldShowHighlightMenuItemHandler != nil) {
-		should_show_highlight_item = self.shouldShowHighlightMenuItemHandler();
-	}
-	if (!should_show_delete_highlight_item && !should_show_highlight_item) {
-		return;
-	}
-
-	NSMenuItem* separator_item = [NSMenuItem separatorItem];
-	separator_item.tag = InkwellDetailHighlightContextMenuSeparatorTag;
-
-	[menu insertItem:separator_item atIndex:0];
-	if (should_show_highlight_item) {
-		SEL highlight_selector = NSSelectorFromString(@"highlightSelectedItem:");
-		NSMenuItem* highlight_item = [[NSMenuItem alloc] initWithTitle:@"Highlight" action:highlight_selector keyEquivalent:@""];
-		highlight_item.target = nil;
-		highlight_item.tag = InkwellDetailHighlightContextMenuItemTag;
-		highlight_item.image = [NSImage imageWithSystemSymbolName:@"highlighter" accessibilityDescription:@"Highlight"];
-		[menu insertItem:highlight_item atIndex:0];
-	}
-	if (should_show_delete_highlight_item) {
-		NSMenuItem* delete_item = [[NSMenuItem alloc] initWithTitle:@"Delete Highlight" action:@selector(deleteHoveredHighlight:) keyEquivalent:@""];
-		delete_item.target = self;
-		delete_item.tag = InkwellDetailDeleteHighlightContextMenuItemTag;
-		[menu insertItem:delete_item atIndex:0];
-	}
-}
-
-- (IBAction) deleteHoveredHighlight:(id) sender
-{
-	#pragma unused(sender)
-	if (self.deleteHoveredHighlightHandler == nil) {
-		return;
-	}
-
-	self.deleteHoveredHighlightHandler();
-}
-
-@end
 
 @interface MBDetailTopBarView : NSVisualEffectView
 @end
